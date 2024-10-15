ccd1630a9cc83d993f998526403bb04c86f898e7
AlexaBF <57883150+AlexaBF@users.noreply.github.com>
2024-10-15 14:48:10 -0600

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import Spline from '@splinetool/react-spline';
+
+export default function App() {
+  return (
+    <main>
+      <Spline
+        scene="https://prod.spline.design/TJJDrM2SqhmykJzW/scene.splinecode"
+        style={{
+          width: '100%',
+          height: '100%',
+        }}
+      />
+    </main>
+  );
+}
